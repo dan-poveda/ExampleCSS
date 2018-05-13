d206af026285d85940c2e4e7211cf31b67ca479c
dany poveda <karendaniela110210@gmail.com>
2018-05-13 11:20:58 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>EJEMPLO HTML | CSS</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="text-center">				
+			<h1>EJEMPLO HTML | CSS </h1>
+		</div>
+	</header>
+	<nav class="nav-left">
+		<ul>
+			<li>
+				<a href="#">Inicio</a>
+			</li>
+			<li>
+				<a href="#">Noticias</a>
+			</li>
+			<li>
+				<a href="#">Intereses</a>
+			</li>
+		</ul>
+	</nav>
+	<main class="container">
+		<section class="section-content">
+			<div class="title-section text-center">
+				<h2>Bienvenidos!!</h2>
+			</div>
+			<article class="article-all article-left">
+				<p>
+					¿Qué es Lorem Ipsum?
+					Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
+				</p>
+			</article>
+			<article class="article-all article-right">
+				<p>
+					¿Por qué lo usamos?
+					Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido aquí". Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de "Lorem Ipsum" va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito (por ejemplo insertándole humor y cosas por el estilo). sadlkfnaslkdnaslkdnmaskdalskdasldn aslkd ask dlkas dlkas dlkas dlk aslkd askl dklas dkl askld klas dkl askld kals dlkas dkl askld klas dkas dlk askld askl dklas dlk askld alks dlkas kld alk dklas dla slkd lkasd  aklsd klas dlkas dlkas dlk asdlk aslkd aslkd lkas dlkas dlkas dlk asdlk aslkd aklsd lkas dlkas dla sdkl sgh.
+				</p>
+			</article>
+		</section>
+	</main>
+	<footer class="text-center">
+		<p>
+			Todos los derechos reservados.
+		</p>
+		<p>
+			© 2018.
+		</p>
+	</footer>
+</body>
+</html>
